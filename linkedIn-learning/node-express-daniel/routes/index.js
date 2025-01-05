50f45a9b80269f10f870cabc265672cec0909e8c
kastastin <kastastin.main@gmail.com>
2025-01-05 20:13:11 +0200

    added templating (express + EJX)

--- linkedIn-learning/node-express-daniel/routes/index.js
+++ linkedIn-learning/node-express-daniel/routes/index.js
@@ -6,14 +6,18 @@ const feedbackRoute = require("./feedback");
 const router = express.Router();
 
 module.exports = (params) => {
-  router.get("/", (req, res) => {
-    // if (!req.session.visitCount) {
-    //   req.session.visitCount = 0;
-    // }
-    // req.session.visitCount += 1;
-    // console.log(`Number of visits: ${req.session.visitCount}`);
+  const { speakersService } = params;
 
-    res.render("pages/index", { pageTitle: "Welcome" });
+  router.get("/", async (req, res) => {
+    const topSpeakers = await speakersService.getList();
+    const artwork = await speakersService.getAllArtwork();
+
+    res.render("layout", {
+      pageTitle: "Welcome",
+      template: "index",
+      topSpeakers,
+      artwork,
+    });
   });
 
   router.use("/speakers", speakersRoute(params));
