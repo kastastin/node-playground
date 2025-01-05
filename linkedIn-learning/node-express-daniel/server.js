8d22eab4898392eca9989c64fd4d861e156293af
kastastin <kastastin.main@gmail.com>
2025-01-05 16:52:43 +0200

    added template engine EJS

--- linkedIn-learning/node-express-daniel/server.js
+++ linkedIn-learning/node-express-daniel/server.js
@@ -4,10 +4,14 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+
 app.use(express.static(path.join(__dirname, "./static")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./static/index.html"));
+  // res.sendFile(path.join(__dirname, "./static/index.html"));
+  res.render("pages/index", { pageTitle: "Welcome" });
 });
 
 app.get("/speakers", (req, res) => {
