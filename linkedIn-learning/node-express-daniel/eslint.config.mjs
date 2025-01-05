bb2ff7ab98ebdcd45a0554cc7b97eb4d8f0866ec
kastastin <kastastin.main@gmail.com>
2025-01-05 15:44:53 +0200

    added basic express app (eslint, prettier, nodemon)

---
+++ linkedIn-learning/node-express-daniel/eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
