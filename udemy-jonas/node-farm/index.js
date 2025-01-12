d54a300f43e689e83770ba3111cebbc637e5e6f0
kastastin <kastastin.main@gmail.com>
2025-01-12 16:02:56 +0200

    routing (node-farm)

--- udemy-jonas/node-farm/index.js
+++ udemy-jonas/node-farm/index.js
@@ -1,7 +1,20 @@
+const url = require("url");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathname = req.url;
+
+  if (pathname === "/" || pathname === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathname === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "custom-header": "custom-header",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
