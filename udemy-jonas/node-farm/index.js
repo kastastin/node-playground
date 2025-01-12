b9e24868d26a8060d6d4719cf3711bb6b1ce3313
kastastin <kastastin.main@gmail.com>
2025-01-12 21:41:20 +0200

    modules, slugify (finish node-farm)

--- udemy-jonas/node-farm/index.js
+++ udemy-jonas/node-farm/index.js
@@ -2,8 +2,14 @@ const fs = require("fs");
 const url = require("url");
 const http = require("http");
 
+const slugify = require("slugify");
+
+const replaceTemplate = require("./modules/replaceTemplate");
+
 const products = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productsObj = JSON.parse(products);
+
+const slugs = productsObj.map((el) => slugify(el.productName, { lower: true }));
 
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -17,23 +23,6 @@ const templateProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8",
 );
-
-function replaceTemplate(template, product) {
-  let output = template.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-}
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
