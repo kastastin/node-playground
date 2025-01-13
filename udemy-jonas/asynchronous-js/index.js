c9a17b47901b09b95dca879fdae545631466bc5d
kastastin <kastastin.main@gmail.com>
2025-01-13 22:25:57 +0200

    building promises

--- udemy-jonas/asynchronous-js/index.js
+++ udemy-jonas/asynchronous-js/index.js
@@ -1,20 +1,34 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-
-      console.log(res.body.message);
-
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        if (err) return console.log(err.message);
-
-        console.log("Image saved to file!");
-      });
+function readFilePromise(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("File not found!");
+      resolve(data);
     });
-});
+  });
+}
+
+function writeFilePromise(file, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("File could not be written!");
+      resolve("Success");
+    });
+  });
+}
+
+readFilePromise(`${__dirname}/dog1.txt`)
+  .then((res) => {
+    console.log(`Breed: ${res}`);
+    return superagent.get(`https://dog.ceo/api/breed/${res}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePromise("dog-img.txt", res.body.message);
+  })
+  .then(() => console.log("Image saved to file!"))
+  .catch((err) => {
+    console.log(err);
+  });
