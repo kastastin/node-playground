bad698c972d949041830da6580a15f693de8a2af
kastastin <kastastin.main@gmail.com>
2025-01-14 18:12:33 +0200

    added POST new tour

--- udemy-jonas/natours/app.js
+++ udemy-jonas/natours/app.js
@@ -7,12 +7,35 @@ const tours = JSON.parse(
 
 const app = express();
 
+// <-- MIDDLEWARE -->
+app.use(express.json());
+
+// <-- GET tours -->
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
     data: { tours },
   });
+});
+
+// <-- POST tours -->
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours.at(-1).id + 1;
+  const newTour = { id: newId, ...req.body };
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: { tour: newTour },
+      });
+    },
+  );
 });
 
 app.listen(3000, () => {
